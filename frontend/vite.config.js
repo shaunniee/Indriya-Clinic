4d9b862a75c9f26ed4ca203320504bc2c1aa9e06
Shaun Vivian Dsouza <shaunvividsz@gmail.com>
2026-02-28 21:24:53 +0000

    v545

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/**
- * Converts render-blocking CSS <link> tags to async preloads.
- * Safe because index.html already inlines all critical above-the-fold CSS.
- */
-function asyncCssPlugin() {
-  return {
-    name: 'async-css',
-    enforce: 'post',
-    transformIndexHtml(html) {
-      return html.replace(
-        /<link rel="stylesheet" crossorigin href="(\/assets\/[^"]+\.css)">/g,
-        '<link rel="preload" as="style" href="$1" crossorigin onload="this.onload=null;this.rel=\'stylesheet\'">' +
-        '<noscript><link rel="stylesheet" crossorigin href="$1"></noscript>'
-      )
-    },
-  }
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), asyncCssPlugin()],
+  plugins: [react()],
   build: {
     // Target browsers with good ES2019+ support:
     // Chrome 87, Firefox 78 (ESR), Safari 14, Edge 88
